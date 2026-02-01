5023629b481202f7b89afeb1b2c2c0cecb62ad66
Arabafenice <95-corda.nasello@icloud.com>
2026-02-01 13:29:16 +0100

    Create selectors.ts

---
+++ offchain/compiler/selectors.ts
@@ -0,0 +1,10 @@
+export const SEL = {
+  modifyLiquidity: "0x5a6bcfda",
+  swap:           "0xf3cd914c",
+  donate:         "0x234266d7",
+  sync:           "0xa5841194",
+  settle:         "0x11da60b4",
+  settleFor:      "0x3dd45adb",
+  take:           "0x0b0d9c09",
+  clear:          "0x80f0b44c",
+} as const;
